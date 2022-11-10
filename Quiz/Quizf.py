b322c11ba8eb99e6c0082f186356df3321bc2617
camilaaa1 <115585056+camilaaa1@users.noreply.github.com>
2022-11-10 17:41:48 -0500

    Add files via upload

---
+++ Quiz/Quizf.py
@@ -0,0 +1,18 @@
+linea = "------------------"
+rios = {'Nilo':'Egipto','Amazonas':'Brasil','Sena':'Francia','Ebro':'España','Volga':'Rusia'}
+
+num = rios.keys()
+pais = rios.values()
+
+for num in reversed(rios):
+    print('El ', str(num) , ' corre a través de ' , str(rios[num]))
+
+print (linea)
+
+for num in reversed(rios):
+        print('Rio ', str(num))
+
+print (linea)
+
+for num in reversed(rios):
+    print('Pais ', str(rios[num]))
---
+++ Quiz/Quizf2.py
@@ -0,0 +1,15 @@
+Linea = "------------"
+Glosario = {"Bucle":"Iteración de un valor seleccionado",
+            "Booleano":"Variable que solo admite dos respuestas",
+            "Arreglo":"'Variable' que almacena distintas variables",
+            "Metodos":"parte de un código que se activa con cierto objeto",
+            "Variables":'"Almacenador" de datos, pueden ser asignados por el código o ser digitados por el usuario dependiendo de su funció'}
+
+num = Glosario.keys()
+for num in reversed(Glosario):
+    print(str(num),":",str(Glosario[num]))
+
+print (Linea)
+
+for num in reversed(Glosario):
+    print(str(num),":","\n", "  ",str(Glosario[num]))
